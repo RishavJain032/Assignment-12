5aaf1626a75d7cc521d3a6cb33589851fee05712
Rishav <rishavj032@gmail.com>
2023-03-30 11:41:39 +0530

    Assignment 12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import User from './Component/User';
+// import Navabar from './Component/Navbar';
+// import container from '../Component/container.css'
+import Main from "./Component/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
+     
+      
     </div>
   );
 }
